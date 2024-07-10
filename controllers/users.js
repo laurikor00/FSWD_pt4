1911ac587e12ec360787b7b52a0079a53b88df70
laurikor00 <84325911+laurikor00@users.noreply.github.com>
2024-07-10 15:26:29 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,13 @@
-const usersRouter = require('express').Router()
+const router = require('express').Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
-usersRouter.get('/', async(request, response) => {
+router.get('/', async(request, response) => {
 	const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, id: 1 })
 	response.json(users)
 })
 
-usersRouter.post('/', async(request, response) => {
+router.post('/', async(request, response) => {
 	const body = request.body
 
 	if (!body.password)
